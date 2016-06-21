fe5c9a0346a94d49055e4117cf91bb158c0ffb0a
Josh Habdas <jhabdas@gmail.com>
2016-06-21 14:44:08 -0500

    Convert root wrappers into pure functions

--- src/main.android.js
+++ src/main.android.js
@@ -2,23 +2,14 @@
  * React Native Webpack Starter Kit
  * https://github.com/jhabdas/react-native-webpack-starter-kit
  */
-import React, { Component, PropTypes } from 'react'
+import React from 'react'
 import { AppRegistry } from 'react-native'
 import App from './components/App'
 
-class Root extends Component {
-  render() {
-    return (<App />)
-  }
-}
+const instructions = 'Shake or press menu button for dev menu'
 
-Root.propTypes = {
-  instructions: PropTypes.string,
-}
-
-Root.defaultProps = {
-  ...App.defaultProps,
-  instructions: 'Shake or press menu button for dev menu',
-}
+const Root = () => (
+  <App instructions={instructions} />
+)
 
 AppRegistry.registerComponent('App', () => Root)
