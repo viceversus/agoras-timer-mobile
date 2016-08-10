d0c68b14b17d4f1e6485433fe96479ab24089172
viceversus <kenshimizu88@gmail.com>
2016-08-10 16:42:10 -0700

    Upgrade to RN .31

---
+++ android/app/src/main/java/com/agoras_timer_mobile/MainActivity.java
@@ -0,0 +1,15 @@
+package com.agoras_timer_mobile;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "agoras_timer_mobile";
+    }
+}
